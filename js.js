7300bc05874efe3eafe1e9be4e6eb3cca83cd07d
Nithz-codes <myselfnithish43@gmail.com>
2026-04-15 20:30:57 +0530

    question and answers has been added

--- js.js
+++ js.js
@@ -1,1 +1,39 @@
 // this file is for javascript
+const question = [
+    {
+        question: "Which is the largest animal in the world?",
+        answers :[
+            {text: "Shark" , correct : false},
+            {text: "Blue Whale" , correct : true},
+            {text: "Elephant" , correct : false},
+            {text: "Giraffe" , correct : false},
+        ]
+    },
+    {
+        question: "Which is the largest desert in the world?",
+        answers :[
+            {text: "Kalahari" , correct : false},
+            {text: "Gobi" , correct : false},
+            {text: "Sahara" , correct : true},
+            {text: "Antartica" , correct : false},
+        ]
+    },
+    {
+        question: "Which is the smallest continent in the world?",
+        answers :[
+            {text: "Asia" , correct : false},
+            {text: "Australia" , correct : true},
+            {text: "Artic" , correct : false},
+            {text: "Africa" , correct : false},
+        ]
+    },
+    {
+        question: "Which is the smallest country in the world?",
+        answers :[
+            {text: "Vatican City" , correct : true},
+            {text: "Bhutan" , correct : false},
+            {text: "Nepal" , correct : false},
+            {text: "Sri Lanka" , correct : false},
+        ]
+    }   
+]
